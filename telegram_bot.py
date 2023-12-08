08c1dfde3499a102ba0147fcf14f9a5498139dcc
Галеев Артур <artgaleev007@gmail.com>
2023-12-08 15:48:07 +0300

    main FTS + bot 1 message update

--- telegram_bot.py
+++ telegram_bot.py
@@ -12,12 +12,17 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     query = message.text  
-    answer = get("http://127.0.0.1:8000/get_comments", params = {'user_text': query})
+    answer = get("http://127.0.0.1:8000/get_comments/", params = {'user_text': query})
     answer = answer.json()
     if answer['message'] == "Not found":
         bot.send_message(message.chat.id, "К сожалению таких комментариев нет(")
     else:
         comments = answer['comments']
+        all_text = "Комментарии:\n"
+
         for comment in comments:
-            bot.send_message(message.chat.id, f"Id комментария - {comment['comment_id']}\nperson_id - {comment['person_id']}\nДата - {comment['date']}\nТекст комментария - {comment['text']}\nId поста - {comment['post_id']}\nКоличество лайков - {comment['likes']}")
+            all_text += f"Id комментария - {comment['comment_id']}\nperson_id - {comment['person_id']}\nДата - {comment['date']}\nТекст комментария - {comment['text']}\nId поста - {comment['post_id']}\nКоличество лайков - {comment['likes']}\n\n"
+        
+        bot.send_message(message.chat.id, all_text)
+
 bot.infinity_polling(none_stop=True)
